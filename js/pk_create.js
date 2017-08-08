c93c31f36d1348ab9c1bbe0e156a787503951d3d
nuotsu <xzmaru@gmail.com>
2017-08-07 23:14:59 -0700

    v2

--- js/pk_create.js
+++ js/pk_create.js
@@ -1,21 +1,39 @@
 $(function() {
-	$('input[type=text]').keyup(write);
-	$('input[type=number], input[type=checkbox], select').change(write);
+	// hover for PPAR
+		$('.ppar').hover(function() {
+			$('.bg').css({
+				'background-image': 'url(\'images/ppar_show.png\')'
+			})
+		}, function() {
+			$('.bg').css({
+				'background-image': 'url(\'images/ppar.png\')'
+			})
+		});
 
-	/* Special */
-		$('.special.optional').after( '<sup>*</sup>' );
-		$('.special.event-only').after( '<sup>**</sup>' );
+	// Upon change of inputs
+		$('input[type=text]').keyup(write);
+		$('input[type=number], input[type=checkbox], select').change(write);
 
-	/* Pokémon */
-		var pkmn = [];
-		$.getJSON('https://nuotsu.github.io/PPAR/js/ppa.json', function(data) {
-            for (var i in data) {
-				pkmn.push(data[i].nameENG);
+	// Special
+		$('.special.optional').after('<sup>*</sup>');
+		$('.special.event-only').after('<sup>**</sup>');
+
+	// Pokémon
+		var pkmnList = [];
+		var ppaList = [];
+		$.getJSON('../PPAR/js/ppa.json', function(ppaJSON) {
+            for (var i in ppaJSON) {
+				if (!ppaJSON[i].name.match(
+						/(Pikachu |Mega|Primal|School|Minior Shield|Original|Egg)/
+					)) {
+					pkmnList.push(ppaJSON[i].name);
+					ppaList.push(ppaJSON[i].dex);
+				}
             }
-			$('#name').autocomplete({
-				source: pkmn
-			});
-			console.log(pkmn);
+			return pkmnList;
+		});
+		$("#pkmn, #gts_pkmn").autocomplete({
+		  source: pkmnList
 		});
 
 	function write() {
@@ -33,7 +51,15 @@ $(function() {
 				} else {
 					serebii = 'N/A';
 				}
-			name = $('#name').val();
+			pkmn = $('#pkmn').val();
+				var ppa = ppaList[pkmnList.indexOf(pkmn)];
+				if (ppa == undefined ||
+					ppa == '0025Co') {
+					ppa = 'Egg';
+				}
+				$('label[for=pkmn] img').attr({
+					'src': 'images/ppa/' + ppa + '_PPA.png'
+				});
 			nickname = $('#nickname').val();
 			nn = $('#nn').val();
 			form = '';
@@ -42,7 +68,36 @@ $(function() {
 				} else {
 					form = 'N/A';
 				}
+				// Forms
+					if (pkmn.indexOf('Alolan') == 0) {
+						$('#form').val('Alolan');
+						form = 'Alolan';
+					} else if (pkmn.indexOf('Lycanroc') == 0) {
+						$('#form').val($('#pkmn').val().replace('Lycanroc ', ''));
+						form = $('#pkmn').val().replace('Lycanroc ', '');
+					} else if (pkmn.indexOf('Minior') == 0) {
+						$('#form').val($('#pkmn').val().replace('Minior ', ''));
+						form = $('#pkmn').val().replace('Minior ', '');
+					} else {
+						$('#form').val('');
+						form = 'N/A';
+					}
 			gender = $('#gender').val();
+				if (pkmn.match(
+						/( F|Nidorina|Nidoqueen)/
+					)) {
+					$('#gender').val('F');
+				} else if (pkmn.match(
+						/( M|Nidorino|Nidoking)/
+					)) {
+					$('#gender').val('M');
+				} else if (pkmn.match(
+						/(Magnemite|Magneton|Voltorb|Electrode)/
+					)) {
+					$('#gender').val('None');
+				} else {
+					$('#gender').val('F');
+				}
 			shiny = '';
 				if ( $('#shiny').prop('checked') ) {
 					shiny = 'Yes';
@@ -64,6 +119,9 @@ $(function() {
 			mtg_loc = $('#mtg_loc').val();
 			mtg_level = $('#mtg_level').val();
 			ball = $('#ball').val();
+				$('label[for=ball] img').attr({
+					'src': 'images/balls/' + ball + '_ball.png'
+				});
 			ribbons = '';
 				if ( $('#ribbons').val() != '' ) {
 					ribbons = $('#ribbons').val();
@@ -95,9 +153,20 @@ $(function() {
 			ign = $('#ign').val();
 			fc = $('#fc').val();
 			gts_pkmn = $('#gts_pkmn').val();
+				var gts_ppa = ppaList[pkmnList.indexOf(gts_pkmn)];
+				if (gts_ppa == undefined ||
+					gts_ppa == '0025Co') {
+					gts_ppa = 'Egg';
+				}
+				$('label[for=gts_pkmn] img').attr({
+					'src': 'images/ppa/' + gts_ppa + '_PPA.png'
+				});
 			gts_gender = $('#gts_gender').val();
 			gts_level = $('#gts_level').val();
 			gts_ball = $('#gts_ball').val();
+				$('label[for=gts_ball] img').attr({
+					'src': 'images/balls/' + gts_ball + '_ball.png'
+				});
 			msg = $('#msg').val();
 		// Form
 			$('textarea').val(
@@ -108,7 +177,7 @@ $(function() {
 				'* **Serebii Link**: ' + serebii + '\n\n' +
 				'&nbsp;' + '\n\n' +
 				'##Pokémon Info ' + '\n\n' +
-				'* **Pokémon**: ' + name + '\n' +
+				'* **Pokémon**: ' + pkmn + '\n' +
 				'* **Nickname**: ' + nickname + '\n' +
 				'* **Form**: ' + form + '\n' +
 				'* **Gender**: ' + gender + '\n' +
